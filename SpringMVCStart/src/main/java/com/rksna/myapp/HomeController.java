9801071da306b9f605b1b9a0da405d3ce4ed5a53
linuxlee <linuxleejang@gmail.com>
2022-09-28 17:39:44 +0900

    테스트 하기위해 model.addAttribute 코드 추가

--- SpringMVCStart/src/main/java/com/rksna/myapp/HomeController.java
+++ SpringMVCStart/src/main/java/com/rksna/myapp/HomeController.java
@@ -33,6 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		model.addAttribute("test", "Hello");
+		model.addAttribute("test1", "Hello1");
 		
 		return "home";
 	}
